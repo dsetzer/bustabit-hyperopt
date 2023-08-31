9a635d8b398c63cdf6aabe408988b8ae8ab136f6
Daniel Setzer <dsetzer@enervare.com>
2023-08-31 05:18:17 +0000

    Fixed semicolon issue with script config parsing

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ from STPyV8 import JSContext, JSObject
 
 class Script:
     def __init__(self, file_path):
-        self.js_code = self.read_js_file(file_path)
-        self.config_dict = self.extract_config_object()
+        raw_js_code = self.read_js_file(file_path)
+        self.config_dict, self.js_code = self.split_config(raw_js_code)
 
     @staticmethod
     def read_js_file(file_path):
@@ -22,14 +22,14 @@ class Script:
                 python_dict[key] = value
         return python_dict
 
-    def extract_config_object(self):
-        start_index = self.js_code.find('var config = {')
+    def split_config(self, raw_js_code):
+        start_index = raw_js_code.find('var config = {')
         if start_index == -1:
             raise FileNotFoundError("Config object not found")
 
         end_index = start_index
         brace_count = 0
-        for i, char in enumerate(self.js_code[start_index:]):
+        for i, char in enumerate(raw_js_code[start_index:]):
             if char == '{':
                 brace_count += 1
             elif char == '}':
@@ -38,12 +38,32 @@ class Script:
                     end_index = start_index + i
                     break
 
+        # Move the end index to the next character after any whitespace or semicolons
+        for i, char in enumerate(raw_js_code[end_index + 1:]):
+            if char.strip():  # Stop at the first non-whitespace character
+                if char == ';':
+                    end_index += i + 1
+                break
+
         # Extract and evaluate only the config object definition
-        config_code = self.js_code[start_index:end_index + 1]
+        config_code = raw_js_code[start_index:end_index + 1]
+        remaining_code = raw_js_code[:start_index] + raw_js_code[end_index + 1:]
 
         with JSContext() as ctxt:
             ctxt.eval(config_code)
             config_object = ctxt.eval("config")
             config_dict = self.js_object_to_python(config_object)
 
-        return config_dict
+        return config_dict, remaining_code
+
+
+    def get_config_js_code(self):
+        # build a string of the config object definition with each item on a new line and defined as a var object.
+        config_code = "var config = {\n"
+        for key in self.config_dict.keys():
+            config_code += f"    {key}: {self.config_dict[key]},\n"
+        config_code += "};\n"
+        return config_code
+
+    def get_combined_js_code(self):
+        return self.get_config_js_code() + self.js_code
